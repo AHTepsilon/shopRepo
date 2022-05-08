d994cfd61dbc5a83257969e382e4d765fc60e41f
AHTepsilon <alehertoro725@gmail.com>
2022-05-08 16:05:12 -0500

    added log out / login functionaloties, trying to fix up shopping cart

---
+++ src/shoppingCart.js
@@ -0,0 +1,97 @@
+import { getAuth, createUserWithEmailAndPassword, onAuthStateChanged } from "firebase/auth";
+import {firebaseConfig, firebase} from "./firebase_app";
+import {app} from "./firebase_app";
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { doc, setDoc } from "firebase/firestore";
+
+import { collection, getDocs } from "firebase/firestore";
+
+import { productArr, displayItems } from "./itemShowcase";
+import {createFirebaseCart, getFirebaseCart} from "./utils/cartFunction";
+
+import { collection, getDoc } from "firebase/firestore";
+
+import { fetchItems } from "./utils/item";
+
+const db = getFirestore(app);
+const auth = getAuth();
+
+const itemsArea = document.getElementById("item_list");
+
+let userHasLoggedIn = undefined;
+let productArr = [];
+let shoppingCart = [];
+
+async function getItems(){
+
+    const firebaseProducts = await fetchItems(db);
+    itemsArea.innerHTML = "";
+    console.log(firebaseProducts);
+
+    firebaseProducts.forEach(product =>{
+
+        renderCartItems(product);
+
+    });
+
+    productArr = firebaseProducts;
+
+}
+
+function renderCartItems(item){
+
+    const cartItem = document.createElement("div");
+    cartItem.className = "cart-item";
+
+    const cartInfoItem = document.createElement("div");
+    cartInfoItem.className = "cart-info";
+
+    const placeholder = item.images ? item.images[0] : "https://www.unfe.org/wp-content/uploads/2019/04/SM-placeholder-1024x512.png";
+
+    const cartHasProducts = shoppingCart.some((productCart) =>{
+
+        productCart.id === item.id;
+
+    });
+
+    cartInfoItem.innerHTML = `
+
+    <h2 class="product_name" id="product_name">${item.nameValue}</h2>
+    <h2 class="product_price" id="product_price">${item.price}.00$</h2>
+
+    `;
+
+    cartItem.innerHTML = `
+    
+    <img src="${placeholder}" class="product_image guitarPhoto" id="product_image>
+
+    `;
+
+    cartItem.appendChild(cartInfoItem);
+    itemsArea.appendChild(cartItem);
+
+}
+
+function getCart(){
+    
+    const localCart = localStorage.getItem("cart");
+    return localCart ? JSON.parse(localCart) : [];
+}
+
+onAuthStateChanged(auth, async (user) =>{
+
+    console.log(user);
+
+    if(user){
+        userHasLoggedIn = user;
+        shoppingCart = await getFirebaseCart(db, userHasLoggedIn.uid);
+        
+    }
+
+    else{
+        shoppingCart = getCart();
+    }
+});
+
+getItems();
---
+++ src/specs/loginLogout.js
@@ -0,0 +1,33 @@
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { getAuth, signOut } from "firebase/auth";
+
+const userButton = document.getElementById("user_button");
+
+const auth = getAuth();
+
+userButton.addEventListener("click", ()=>{
+
+    onAuthStateChanged(auth, (user) => {
+        if (user) {
+          const uid = user.uid;
+      
+          const auth = getAuth();
+          signOut(auth).then(() => {
+
+            window.location.href = "./index.html";
+            alert("Signed out succesfully");
+            
+          }).catch((error) => {
+           
+            console.log(error);
+
+          });
+      
+        } else {
+          
+            window.location.href = "./loginPage.html";
+      
+        }
+      });
+
+});
