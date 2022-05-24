b65098cf5cf405fd6472bceb9092026b64861df2
AHTepsilon <alehertoro725@gmail.com>
2022-05-24 18:25:53 -0500

    implemented p5js and tonejs libraries correctly

--- anim/sketch.js
+++ anim/sketch.js
@@ -1,7 +1,33 @@
-function setup() {
-  createCanvas(windowWidth, windowHeight);
+import * as Tone from 'tone'
+
+let startAudioBtn = document.getElementById("startAudioButton");
+
+let loopBeat;
+let bassSynth;
+
+window.setup = function setup() {
+  let canv = createCanvas(1080, 720);
+
+  alert("Please click the start audio button if there's no sound playing");
+
+  bassSynth = new Tone.MembraneSynth().toDestination();
+
+  loopBeat = new Tone.Loop(song, "4n");
+  Tone.Transport.start();
+  loopBeat.start();
 }
 
-function draw() {
-  
+window.draw = function draw() {
+  background(255, 0, 0);
+}
+
+startAudioBtn.addEventListener("click", async ev =>{
+
+  await Tone.start();
+  alert("Audio is ready");
+
+});
+
+function song(time){
+  bassSynth.triggerAttackRelease("c1", "8n", time);
 }
--- dist/index.03f57f4d.js
+++ dist/index.03f57f4d.js
@@ -1,6 +1,8 @@
 function setup() {
-    createCanvas(windowWidth, windowHeight);
+    let canv = createCanvas(1080, 720);
 }
-function draw() {}
+function draw() {
+    background(255, 0, 0);
+}
 
 //# sourceMappingURL=index.03f57f4d.js.map
